2c5a03f1e3410070db42b51a27eaff01ee0cc66d
fasil <45447375+fasilbeshiwork@users.noreply.github.com>
2021-08-16 16:16:22 +0300

    Drivers Login

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute:MainScreen.idScreen,
+      initialRoute:LoginScreen.idScreen,
       routes:
     {
        SigninScreen.idScreen: (context) => SigninScreen(),
